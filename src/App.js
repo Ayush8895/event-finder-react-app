982e4f9d603c48fcf745549f9221c99b40d63b53
Ayush Kumar <ayushbargarh2@gmail.com>
2022-07-03 22:14:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import './App.css';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import CreateEvent from './components/CreateEvent';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import React, { useState } from "react";
+
+
+const App = () => {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+const [eventList, setEventList] = useState([])
+const saveEvent = (eventObj) => {
+  let tempList = eventList
+  tempList.push(eventObj)
+  setEventList(tempList)
+}
+  return (<>
+  <Navbar/>
+  <CreateEvent/>
+  <Footer/>
+
+  </>
   );
 }
 
